00aea2a270cfee6a4b698439552c7d27ca54ca39
kevinbird61 <kevinbird61@gmail.com>
2017-06-07 16:58:57 +0000

    Update device station

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // lib
+// const sender = require('./deliver');
+const qs = require('querystring');
 const http = require('http');
-const url = require('url');
-// const router = require('./router');
-const sender = require('./delivery');
 const mraa = require('mraa');
 var uart = new mraa.Uart(1); // Using port 16,17 to do the transport
 
@@ -12,8 +11,8 @@ uart.setMode(8,0,1);
 uart.setFlowcontrol(false,false);
 var strline = "";
 
-/* Disable the http server , change to active emit http request to server.
-function onRequest(req,res){
+// Disable the http server , change to active emit http request to server.
+/*function onRequest(req,res){
     // router
     var pathname = url.parse(req.url).pathname;
     if(req.method == "POST"){
@@ -26,17 +25,55 @@ console.log("[SVIM] base-station");
 */
 
 console.log("Ready to get the data from the MCU!");
+
 while(1){
     if(uart.dataAvailable(2000)){
         var judge = uart.readStr(1024);
         if(judge != "#"){
-            total = total + judge;
+            strline = strline + judge;
         }
         else{
-            console.log(total);
+            console.log(strline);
             // TODO: Using sender to deliver the message which collect from MCU
+			const obj = qs.stringify({
+				'id': "UUID",
+				'sync_data': strline
+			});
+			
+			const http_options = {
+				hostname: "140.116.245.247",
+				port: 3000,
+				path: '/data_sync',
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Content-Length': Buffer.byteLength(obj)
+				}
+			};
+			
+			const http_req = http.request(http_options, function(res){
+				console.log("Status: " + res.statusCode);
+				console.log("Headers: " + JSON.stringify(res.headers));
+				res.setEncoding('utf8');
+				res.on('data',function(chunk){
+					console.log(chunk);
+				});
+				res.on('end',function(){
+					console.log("No more data in response.");
+				});
+			});
+
+			http_req.on('error',function(e){
+				console.error('Problem with request: ' + e.message);
+			});
+
+			http_req.write(obj);
+			http_req.end();
+
             uart.writeStr("OK");
-            total = "";
+            strline = "";
+
+			return;
         }
         // process.stdout.write(uart.readStr(1024));
     }
--- deliver.js
+++ deliver.js
@@ -2,17 +2,20 @@ const qs = require('querystring');
 const http = require('http');
 
 // Deliver code
-function delivery(rawdata){
+function delivery(id,rawdata){
     // Build the post string from an object
+	console.log("Get data: " + rawdata + ", which id is: " + id );
+
   var post_data = qs.stringify({
+	  'id': id,
       'sync_data': rawdata
   });
 
   // An object of options to indicate where to post to
   var post_options = {
-      host: 'localhost',
+      host: '140.116.245.247',
       port: '3000',
-      path: '/restful_api',
+      path: '/data_sync',
       method: 'POST',
       headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -22,17 +25,25 @@ function delivery(rawdata){
 
   // Set up the request
   var post_req = http.request(post_options, function(res) {
+		console.log("Status: " + res.statusCode);
+		console.log("Headers: " + JSON.stringify(res.headers));
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
-          console.log('Get serer Response: ' + chunk);
+          console.log('Get server Response: ' + chunk);
       });
+	  res.on('end',function(){
+		  console.log('No more data in response.');			
+	  })
   });
 
+	post_req.on('error',function(e){
+		console.error("problem with request: " + e.message);
+	});
   // post the data
   post_req.write(post_data);
   post_req.end();
 }
 
 module.exports = {
-    delivery: delivery
+    getSync: delivery
 }
