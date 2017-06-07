52bc337830c0c4165464025f9923e07fd9aba65e
kevinbird61 <kevinbird61@gmail.com>
2017-06-08 01:06:19 +0800

    Rearrange the file.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // lib
-// const sender = require('./deliver');
+const sender = require('./deliver');
 const qs = require('querystring');
 const http = require('http');
 const mraa = require('mraa');
@@ -10,19 +10,6 @@ uart.setBaudRate(57600);
 uart.setMode(8,0,1);
 uart.setFlowcontrol(false,false);
 var strline = "";
-
-// Disable the http server , change to active emit http request to server.
-/*function onRequest(req,res){
-    // router
-    var pathname = url.parse(req.url).pathname;
-    if(req.method == "POST"){
-        console.log("POST method: "+pathname);
-    }
-    router.parsing(pathname,req,res);
-}
-http.createServer(onRequest).listen(process.env.npm_package_config_port);
-console.log("[SVIM] base-station");
-*/
 
 console.log("Ready to get the data from the MCU!");
 
@@ -35,45 +22,16 @@ while(1){
         else{
             console.log(strline);
             // TODO: Using sender to deliver the message which collect from MCU
-			const obj = qs.stringify({
-				'id': "UUID",
-				'sync_data': strline
-			});
-			
-			const http_options = {
-				hostname: "140.116.245.247",
-				port: 3000,
-				path: '/data_sync',
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-					'Content-Length': Buffer.byteLength(obj)
-				}
-			};
-			
-			const http_req = http.request(http_options, function(res){
-				console.log("Status: " + res.statusCode);
-				console.log("Headers: " + JSON.stringify(res.headers));
-				res.setEncoding('utf8');
-				res.on('data',function(chunk){
-					console.log(chunk);
-				});
-				res.on('end',function(){
-					console.log("No more data in response.");
-				});
-			});
-
-			http_req.on('error',function(e){
-				console.error('Problem with request: ' + e.message);
-			});
-
-			http_req.write(obj);
-			http_req.end();
+            sender.getSync('UIDD',strline,function(err,msg){
+                if(err)
+                    console.log(msg);
+                else{
+                    console.log("Successfully deliver message to server.");
+                }
+            });
 
             uart.writeStr("OK");
             strline = "";
-
-			return;
         }
         // process.stdout.write(uart.readStr(1024));
     }
--- deliver.js
+++ deliver.js
@@ -2,48 +2,44 @@ const qs = require('querystring');
 const http = require('http');
 
 // Deliver code
-function delivery(id,rawdata){
-    // Build the post string from an object
-	console.log("Get data: " + rawdata + ", which id is: " + id );
+function getSync(id,rawdata,callback){
+	const obj = qs.stringify({
+		'id': "UUID",
+		'sync_data': rawdata
+	});
 
-  var post_data = qs.stringify({
-	  'id': id,
-      'sync_data': rawdata
-  });
+	const http_options = {
+		hostname: "140.116.245.247",
+		port: 3000,
+		path: '/data_sync',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(obj)
+		}
+	};
 
-  // An object of options to indicate where to post to
-  var post_options = {
-      host: '140.116.245.247',
-      port: '3000',
-      path: '/data_sync',
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': Buffer.byteLength(post_data)
-      }
-  };
-
-  // Set up the request
-  var post_req = http.request(post_options, function(res) {
+	const http_req = http.request(http_options, function(res){
 		console.log("Status: " + res.statusCode);
 		console.log("Headers: " + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          console.log('Get server Response: ' + chunk);
-      });
-	  res.on('end',function(){
-		  console.log('No more data in response.');			
-	  })
-  });
-
-	post_req.on('error',function(e){
-		console.error("problem with request: " + e.message);
+		res.setEncoding('utf8');
+		res.on('data',function(chunk){
+			console.log(chunk);
+		});
+		res.on('end',function(){
+			console.log("No more data in response.");
+			callback(0,"No more data in response.");
+		});
 	});
-  // post the data
-  post_req.write(post_data);
-  post_req.end();
+
+	http_req.on('error',function(e){
+		console.error('Problem with request: ' + e.message);
+	});
+
+	http_req.write(obj);
+	http_req.end();
 }
 
 module.exports = {
-    getSync: delivery
+    getSync: getSync
 }
